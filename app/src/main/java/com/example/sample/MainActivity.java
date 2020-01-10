13663a75af070f186bae3e6fb2a449786de3a642
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-10 17:19:43 +0900

    Initial commit

---
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //main(String[] args)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
