68f79f3e38ea64f812df44b7dd3437b5332482b0
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-14 19:17:57 +0900

    Iasa Login layout improved

--- app/src/main/java/com/example/sample/IasaActivity.java
+++ app/src/main/java/com/example/sample/IasaActivity.java
@@ -11,15 +11,23 @@ import android.widget.RelativeLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class IasaActivity extends AppCompatActivity {
 
+    TextInputLayout inputLayout;
+    TextInputEditText inputEditText;
     //main(String[] args)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iasa);
 
+        inputLayout = findViewById(R.id.TextInputLayout_password);
+        inputEditText = findViewById(R.id.TextInputEditText_password);
+
+        inputLayout.setPasswordVisibilityToggleEnabled(true);
+        inputEditText.clearAnimation();
 
     }
 }
